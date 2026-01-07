400ab9c6a5d5e228a15111ac52bc2541979c1c25
Mr-c0ki <yudiahmad8080@gmail.com>
2026-01-07 13:52:44 +0700

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "[*] Installing Mantis-Tools..."
+go build -o scanner scanner.go
+pip3 install rich requests paramiko --quiet
+current_dir=$(pwd)
+cat <<EOF > mantis-tools
+#!/bin/bash
+python3 $current_dir/mantis.py "\$@"
+EOF
+chmod +x mantis-tools
+chmod +x scanner
+sudo mv mantis-tools /usr/local/bin/
+echo "[+] Done. Usage: mantis-tools -t <target> -e"
